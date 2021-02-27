f74f7fa12603a7143344dd092c916a8da69167e9
Bohmaster <voidhmaster@gmail.com>
2021-02-27 01:50:45 -0300

    Mysql response.

--- web-server.js
+++ web-server.js
@@ -19,7 +19,12 @@ app.use(bodyParser.json());
 app.post('/webhook', (req, res) => {
  console.log(1, req.body.type, req.body.payload.payload.text);   
  if (req.body.type === "message" && req.body.payload.payload.text === "1") {
-       res.send("1- Carlos Bustamante (Clínico), 2- Martin Rojas (Kiensiólogo)");
+        connection.query('SELECT * from Medicos', function (error, results, fields) {
+        if (error) throw error;
+        // results.forEach(result => {console.log("Medico nombre: ", result.Descripcion)});
+        res.send("respuesta: ", results[0].Descripcion);
+      }); 
+       
     } else {
        res.send("Bienvenido al sistema de turnos. Escriba 1 para ver los médicos");
     }
