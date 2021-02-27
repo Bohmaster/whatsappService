f593b955fe3e3149f446ab0bc9ce1af6a1649c32
Bohmaster <voidhmaster@gmail.com>
2021-02-27 01:35:51 -0300

    db

--- web-server.js
+++ web-server.js
@@ -2,40 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const csv = require('csv-parser');
 const fs = require("fs");
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'laravel',
+  password : 'laravel',
+  database : 'c1990040_Turnos'
+});
+
+connection.connect();
 
 const app = express();
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-let medicos = [];
-let especialidades = [];
-
-fs.createReadStream('Especialidad.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-        especialidades.push(row)
-    })
-    .on('end', () => {
-        console.log('CSV file successfully processed');
-
-        fs.createReadStream('Medicos.csv')
-            .pipe(csv())
-            .on('data', (row2) => {
-                medicos.push(row2);
-
-            })
-            .on('end', () => {
-                console.log('CSV file successfully processed');
-            });
-    });
-
 app.post('/webhook', (req, res) => {
-    console.log(req.body, req.params, req.data);
-
-    console.log(especialidades[0], medicos[0]);
-
-    res.send("Great")
+ console.log(1, req.body.type, req.body.payload.payload.text);   
+ if (req.body.type === "message" && req.body.payload.payload.text === "1") {
+       res.send("1- Carlos Bustamante (Clínico), 2- Martin Rojas (Kiensiólogo)");
+    } else {
+       res.send("Bienvenido al sistema de turnos. Escriba 1 para ver los médicos");
+    }
 });
 
 app.listen(3004, ()=> {
